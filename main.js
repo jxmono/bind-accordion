1a8efb5446e7fff0c60f3acc3eeb0f8757dfc036
Ionica Bizau <bizauionica@yahoo.com>
2013-05-03 18:20:27 +0300

    First working version of bind-accordion module.

--- main.js
+++ main.js
@@ -2,12 +2,75 @@ module.exports = function (config) {
 
     config = processConfig(config);
 
-    // More code in the next commit.
+    // TODO Clean the code
+
+    // TODO Set initial state on page load
+    // using cookies?
+
+    // Click on list header
+    // $(config.listHeader).delegate("click", function() {
+    setTimeout(function() {
+        $(config.listHeader).on("click", function() {
+            var clickedList = $(this);
+
+            switch (config.list.type) {
+                
+                // Is a jQuery function
+                case "jQueryFunction":
+                    var icons = config.icons;
+                    var list = clickedList[config.list.value]();
+
+                    // The list is hidden
+                    if (list.css("display") == "none"){
+                        
+                        switch (config.icons.type) {
+                            case "class":
+                                clickedList.find(icons.container).removeClass(icons.hidden);
+                                clickedList.find(icons.container).addClass(icons.visible);
+                            
+                            break;
+                            // TODO Check for errors
+                        }
+
+                    // The list is visible
+                    } else {
+                        switch (config.icons.type) {
+                            case "class":
+                                clickedList.find(icons.container).addClass(icons.hidden);
+                                clickedList.find(icons.container).removeClass(icons.visible);
+                            
+                            break;
+                            // TODO Check for errors
+                        }
+                    }
+                    list.toggle(function() {
+                        
+                    });
+                    break;
+            }
+        });
+    }, 2000);
 }
 
+/*
+    "config": {
+        "listHeader": ".projectFolder",
+        "list": {
+            "type": "jQueryFunction",
+            "value": "next"
+        },
+        "icons": {
+            "type": "class"
+            "visible": "icon-chevron-down"
+            "hidden": "icon-chevron-right"
+            "container": ".icon"
+        }
+    }
+*/
 
 function processConfig(config) {
-    
+
+    // TODO Build function
     
     return config;
 }
